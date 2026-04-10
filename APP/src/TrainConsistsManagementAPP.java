3164225fa69ea2787141eecbb61a200b7a5eff92
Hareesh1906 <kondurisaihareesh76@gmail.com>
2026-04-10 16:05:11 +0530

    [HAREESH] ADDED  UC19-Binary-Search-for-Bogie-ID

--- APP/src/TrainConsistsManagementAPP.java
+++ APP/src/TrainConsistsManagementAPP.java
@@ -11,53 +11,52 @@ public class TrainConsitsMangementApp {
         int n = sc.nextInt();
         sc.nextLine(); // consume newline
 
-        String[] bogieNames = new String[n];
+        String[] bogieIDs = new String[n];
 
-        // Input bogie names
+        // Input bogie IDs
         for (int i = 0; i < n; i++) {
-            System.out.print("Enter Bogie Name: ");
-            bogieNames[i] = sc.nextLine();
-        }import java.util.Scanner;
+            System.out.print("Enter Bogie ID: ");
+            bogieIDs[i] = sc.nextLine();
+        }
 
-        public class TrainConsitsMangementApp {
+        // Sort array before binary search
+        Arrays.sort(bogieIDs);
 
-            public static void main(String[] args) {
+        System.out.println("\nSorted Bogie IDs:");
+        for (String id : bogieIDs) {
+            System.out.println(id);
+        }
 
-                Scanner sc = new Scanner(System.in);
+        // Search input
+        System.out.print("\nEnter Bogie ID to search: ");
+        String searchID = sc.nextLine();
 
-                System.out.print("Enter number of bogies: ");
-                int n = sc.nextInt();
-                sc.nextLine(); // consume newline
+        int left = 0;
+        int right = n - 1;
+        boolean found = false;
 
-                String[] bogieIDs = new String[n];
+        // Binary Search
+        while (left <= right) {
 
-                // Input bogie IDs
-                for (int i = 0; i < n; i++) {
-                    System.out.print("Enter Bogie ID: ");
-                    bogieIDs[i] = sc.nextLine();
-                }
+            int mid = (left + right) / 2;
 
-                // Search element
-                System.out.print("\nEnter Bogie ID to search: ");
-                String searchID = sc.nextLine();
+            int compare = bogieIDs[mid].compareToIgnoreCase(searchID);
 
-                boolean found = false;
-
-                // Linear Search
-                for (int i = 0; i < n; i++) {
-                    if (bogieIDs[i].equalsIgnoreCase(searchID)) {
-                        System.out.println("Bogie ID FOUND at position: " + (i + 1));
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found) {
-                    System.out.println("Bogie ID NOT FOUND!");
-                }
-
-                sc.close();
+            if (compare == 0) {
+                System.out.println("Bogie ID FOUND at position: " + (mid + 1));
+                found = true;
+                break;
             }
+            else if (compare < 0) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Bogie ID NOT FOUND!");
         }
 
         sc.close();
